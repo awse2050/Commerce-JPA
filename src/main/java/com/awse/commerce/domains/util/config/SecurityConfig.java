963ceec430519fc5527fffb0ac8967603b6fe895
kyh29 <kyh3964@naver.com>
2021-05-19 15:11:15 +0900

    add

---
+++ src/main/java/com/awse/commerce/domains/util/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.awse.commerce.domains.util.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable();
+        http.formLogin();
+        http.logout();
+    }
+
+    // PasswordEncoder
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
