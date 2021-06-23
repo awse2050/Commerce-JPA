72a9a4d1167c096537a860986543d7354e89fb40
kyh29 <kyh3964@naver.com>
2021-06-23 18:43:45 +0900

    add Select Remove API at Cart table

--- src/main/java/com/awse/commerce/domains/cart/dao/ModifyRequestItemDao.java
+++ src/main/java/com/awse/commerce/domains/cart/dao/ModifyRequestItemDao.java
@@ -15,6 +15,6 @@ public class ModifyRequestItemDao {
     // 상품번호, 주문수량
     private Long itemId;
     // 최소 1이상
-    @Min(value = 1, message = "1개 이상 입력해주세요.")
+    @Min(value = 1, message = "최소 1개 이상 입력해주세요.")
     private int orderCount;
 }
